417f7fe893c7425c23b6b69eccda172d49b24744
maeflor <maeflor.butawan@bisu.edu.ph>
2023-11-30 14:52:07 +0800

    2nd commit

---
+++ infos.js
@@ -0,0 +1,101 @@
+export const datas = [
+    {
+      id:'1',
+      name: 'JOHN JALA',
+      nickName:'doy',
+      address:'South America',
+      courseAndYear:'BSIT-3',
+      avatar:
+        'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+    },
+    {
+        id:'2',
+        name: 'MRJOLINE EGAMA',
+        nickName:'jojow',
+        address:'Lila',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'3',
+        name: 'PEPITO WENG',
+        nickName:'pet',
+        address:'London',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'4',
+        name: 'JEEVE LUNZON',
+        nickName:'lyn',
+        address:'Dampas',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'5',
+        name: 'ROWENA SUIZO',
+        nickName:'wena',
+        address:'Antequera',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'6',
+        name: 'REA GALEROS',
+        nickName:'bossing',
+        address:'Loboc',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'7',
+        name: 'NIKKO ALTURAS',
+        nickName:'nikkos',
+        address:'Mars',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'8',
+        name: 'EMJAY SANCHEZ',
+        nickName:'mj',
+        address:'Dao',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'9',
+        name: 'AVERY BUTAWAN',
+        nickName:'ave',
+        address:'Sikatuna',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'10',
+        name: 'FLORENCE INTONG',
+        nickName:'mark',
+        address:'Tagbilaran',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'11',
+        name: 'DAVE BAUGBOG',
+        nickName:'dabby',
+        address:'Danicop',
+        courseAndYear:'BSIT-3',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+  ];
